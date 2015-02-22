a3365e706883aa57a4ba5229e15d435c35e6c81f
aswitalski <aleksander.switalski@gmail.com>
2015-02-22 18:35:00 +0100

    Configured static resources handlers

--- src/main/java/com/sky/assignment/web/init/SkyWebAppConfig.java
+++ src/main/java/com/sky/assignment/web/init/SkyWebAppConfig.java
@@ -3,6 +3,7 @@ package com.sky.assignment.web.init;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,4 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan("com.sky.assignment")
 public class SkyWebAppConfig extends WebMvcConfigurerAdapter {
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("index.html").addResourceLocations("index.html");
+		registry.addResourceHandler("images/**").addResourceLocations("images/**");
+		registry.addResourceHandler("scripts/**").addResourceLocations("scripts/**");
+		registry.addResourceHandler("styles/**").addResourceLocations("styles/**");
+		registry.addResourceHandler("views/**").addResourceLocations("views/**");
+	}
 }
